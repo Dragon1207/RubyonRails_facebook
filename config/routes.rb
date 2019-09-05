9318ab32efd09e83515b324b7654a520ba943523
Dragon1207 <smileboth0805@outlook.com>
2019-09-05 23:08:50 +0200

    Add UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#home"
   devise_for :users
+  resources :users, only:[:index, :show]
 end
