ced3de3706c0f4ba80051253211d611a01753b6d
Dragon1207 <smileboth0805@outlook.com>
2019-09-09 18:45:52 +0200

    Fix issue in FriendRequests when no user is logged in

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,6 +1,10 @@
 class FriendRequestsController < ApplicationController
   def index
-    @friend_offers = current_user.friend_offers
-    @friend_requests = current_user.friend_requests
+    unless current_user.nil?
+      @friend_offers = current_user.friend_offers
+      @friend_requests = current_user.friend_requests
+    else
+      @friend_offers = @friend_requests = FriendRequest.none
+    end
   end
 end
