877fe261aae6886aa94367a3c8e8d65f821f84ce
Dragon1207 <smileboth0805@outlook.com>
2019-09-16 20:36:38 +0200

    Add navigation bar to layout

---
+++ test/integration/navbar_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class NavbarTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  include ActionView::Helpers::TextHelper
+
+  test "shouldn't show navbar when no user is signed in" do
+    get root_path
+    assert_select "a[href=?]", root_path, count: 0 # home link
+    assert_select "a[href=?]", users_path, count: 0 # find friends link
+    assert_select "a[href=?]", destroy_user_session_path, count: 0 # sign out link
+  end
+
+  test "should show navbar when a user is signed in" do
+    alice = users(:alice)
+    sign_in alice
+    get root_path
+    assert_select "a[href=?]", root_path, count: 1 # home link
+    assert_select "a[href=?]", users_path, count: 1 # find friends link
+    assert_select "a[href=?]", destroy_user_session_path, count: 1 # sign out link
+    assert_select "a[href=?]", user_path(alice), alice.name # profile link
+    # friend requests notification link
+    assert_select "a[href=?]", friend_requests_path,
+                               "(#{alice.friend_offers.count}) Friend #{"Request".pluralize(alice.friend_offers.count)}"
+  end
+end
