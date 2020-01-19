0e9ecf861886da53a67e827996800a5b0d068631
Dragon1207 <smileboth0805@outlook.com>
2020-01-19 22:18:28 +0100

    Update post index page
    Show author name, date of creation and like button for each post,
    along with post's (text) contents.

---
+++ test/integration/post_index_timeline_feed_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class PostIndexTimelineFeedTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @alice = users(:alice)
+  end 
+
+  test "post index page" do
+    ## should redirect when not signed in
+    get posts_path
+    assert_response :redirect
+    assert_redirected_to new_user_session_url
+    follow_redirect!
+
+    sign_in @alice
+
+    get posts_path
+    assert_response :success
+
+    ## should display feed
+    assert_select '.post-display'
+    assert_select '.post-content'
+    feed = @alice.post_feed.limit(10)
+    feed.each do |post|
+      assert_match CGI::escapeHTML(post.author.name), response.body
+      assert_match CGI::escapeHTML(post.text), response.body
+    end
+    # and like button and comment button
+    assert_select '.post-buttons > form'
+  end
+end
