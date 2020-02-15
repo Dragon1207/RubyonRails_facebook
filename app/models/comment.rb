4e196a3259758aac79b780cbade62f0ace45a89b
Dragon1207 <smileboth0805@outlook.com>
2020-02-15 19:46:44 +0100

    Add ordering to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: "User"
 
+  default_scope -> { order(created_at: :asc) }
+
   validates :text, presence: true
 end
