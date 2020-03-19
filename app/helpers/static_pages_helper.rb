3efddbbb91723e25120f844998e848e4d4852133
Dragon1207 <smileboth0805@outlook.com>
2020-03-19 22:00:58 +0100

    Add sign-up form to homepage
    Embedded devise sessions view into home view, to mimic FB's design.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,17 @@
 module StaticPagesHelper
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_name
+    :user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_class
+    Struct.new(:omniauth_providers).new(['facebook'])
+  end
 end
