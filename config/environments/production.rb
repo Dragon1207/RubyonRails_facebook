e488de63f21af568a7557acc8127dc5e4bc6306f
Dragon1207 <smileboth0805@outlook.com>
2021-04-04 21:29:38 +0200

    Update site addres for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   # SendGrid config
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mighty-forest-49229.herokuapp.com'
+  host = 'phacepook.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
